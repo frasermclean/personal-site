e560b7b86158ced14899956838a9048fbda5d7df
Fraser McLean <dev@frasermclean.com>
2024-08-14 17:29:07 +0800

    Move Hugo project to root and remove backend project

--- backend/Contracts/Requests/SendEmailRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonalSite.Backend.Contracts.Requests;
-
-public class SendEmailRequest
-{
-    public string FromName { get; init; } = string.Empty;
-    public string FromAddress { get; init; } = string.Empty;
-    public string Subject { get; init; } = string.Empty;
-    public string Body { get; init; } = string.Empty;
-}
