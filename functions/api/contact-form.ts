40d92b6378220415dc730cf1b0d84e0fd810f1ce
Fraser McLean <dev@frasermclean.com>
2024-08-16 10:15:46 +0800

    Implement email sending via Resend API

--- functions/api/contact-form.ts
+++ functions/api/contact-form.ts
@@ -1,13 +1,22 @@
+import { Resend } from 'resend';
+
+interface Env {
+  CONTACT_ADDRESS: string;
+  RESEND_API_KEY: string;
+}
+
 interface MessageData {
+  to: string;
   name: string;
   email: string;
   message: string;
 }
 
-export const onRequestPost: PagesFunction = async (context) => {
+export const onRequestPost: PagesFunction<Env> = async (context) => {
   // extract message data from the form
   const formData = await context.request.formData();
   const messageData = {
+    to: context.env.CONTACT_ADDRESS,
     name: formData.get('name'),
     email: formData.get('email'),
     message: formData.get('message'),
@@ -21,7 +30,7 @@ export const onRequestPost: PagesFunction = async (context) => {
   }
 
   // send email
-  const isSuccess = await sendEmail(messageData);
+  const isSuccess = await sendEmail(context.env.RESEND_API_KEY, messageData);
   if (!isSuccess) {
     return new Response('Failed to send email', { status: 500 });
   }
@@ -50,6 +59,28 @@ function validateData(data: MessageData): string {
   return '';
 }
 
-async function sendEmail(data: MessageData): Promise<boolean> {
-  return false;
+/**
+ * Send an email using the Resend API
+ * @param apiKey The Resend API key
+ * @param data Message data
+ * @returns True if the email was sent successfully
+ */
+async function sendEmail(apiKey: string, data: MessageData): Promise<boolean> {
+  const resend = new Resend(apiKey);
+
+  const response = await resend.emails.send({
+    from: 'Contact Form <no-reply@resend.dev>',
+    to: data.to,
+    reply_to: data.email,
+    subject: `Message from ${data.name}`,
+    text: data.message,
+  })
+
+  if (response.error) {
+    console.error('Error sending email', response.error);
+    return false;
+  }
+
+  console.log('Email sent successfully', response.data.id);
+  return true;
 }
