8fa7c05b4413bccb3874b98b743e0e42643a6571
Fraser McLean <dev@frasermclean.com>
2025-11-07 19:41:58 +0800

    Add sitemap integration and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/lib/reading-time.js';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://next.frasermclean.com',
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
