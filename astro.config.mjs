8626097763925c6bde3420d560513383edca405c
Fraser McLean <dev@frasermclean.com>
2025-11-09 13:43:39 +0800

    Add Cloudflare Astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,26 @@
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/lib/reading-time.js';
 
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://next.frasermclean.com',
+  adapter: cloudflare({
+    imageService: 'compile'
+  }),
   integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
       themes: {
         light: 'light-plus',
-        dark: 'dark-plus',
-      },
-    },
+        dark: 'dark-plus'
+      }
+    }
   },
   vite: {
-    plugins: [tailwindcss()],
-  },
+    plugins: [tailwindcss()]
+  }
 });
