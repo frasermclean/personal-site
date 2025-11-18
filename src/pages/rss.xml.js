b47517d2f4e7bb9d461dda906e6c25cf1cf9a82a
Fraser McLean <dev@frasermclean.com>
2025-11-18 15:55:33 +0800

    Add RSS feed generation for blog posts

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import { SITE_DESCRIPTION, SITE_TITLE } from '@/constants';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = (await getCollection('posts')).sort(
+    (a, b) => new Date(b.data.publishDate) - new Date(a.data.publishDate)
+  );
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.publishDate,
+      link: `/posts/${post.id}`
+    })),
+    stylesheet: '/pretty-feed-v3.xsl'
+  });
+}
