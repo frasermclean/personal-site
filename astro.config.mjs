8c5bb6bc6512624cc6437e25bfff7d7d03f0d770
Fraser McLean <dev@frasermclean.com>
2026-03-14 10:06:40 +0100

    Add default values for TURNSTILE_SITE_KEY and TURNSTILE_SECRET_KEY in environment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,8 +36,12 @@ export default defineConfig({
       COMMENTS_HOST: envField.string({ context: 'client', access: 'public', default: 'http://localhost:8080' }),
       CONTACT_EMAIL: envField.string({ context: 'server', access: 'secret' }),
       RESEND_API_KEY: envField.string({ context: 'server', access: 'secret' }),
-      TURNSTILE_SITE_KEY: envField.string({ context: 'client', access: 'public' }),
-      TURNSTILE_SECRET_KEY: envField.string({ context: 'server', access: 'secret' }),
+      TURNSTILE_SITE_KEY: envField.string({ context: 'client', access: 'public', default: '1x00000000000000000000AA' }),
+      TURNSTILE_SECRET_KEY: envField.string({
+        context: 'server',
+        access: 'secret',
+        default: '1x0000000000000000000000000000000AA'
+      }),
       WORKERS_CI_COMMIT_SHA: envField.string({ context: 'client', access: 'public', default: '' })
     }
   },
