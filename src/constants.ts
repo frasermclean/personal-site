1f66499ba26fb56bdb20f00baee489d3402b8353
Fraser McLean <dev@frasermclean.com>
2026-05-06 12:40:11 +0100

    Refactor navigation links: move links definition to NavigationLinks.astro and remove from constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,3 @@
-import ArticleIcon from '@tabler/icons/outline/article.svg';
-import ChecklistIcon from '@tabler/icons/outline/checklist.svg';
-import HomeIcon from '@tabler/icons/outline/home.svg';
-import MailIcon from '@tabler/icons/outline/mail.svg';
-import UserIcon from '@tabler/icons/outline/user.svg';
-
 export const SITE_URL = 'https://frasermclean.com';
 export const SITE_TITLE = 'Fraser McLean';
 export const SITE_DESCRIPTION = 'The personal website of Fraser McLean - full-stack developer and tech enthusiast.';
@@ -17,11 +11,3 @@ export const PROJECTS_PATH = '/projects';
 export const POSTS_PATH = '/posts';
 export const CONTACT_PATH = '/contact';
 export const TAGS_PATH = '/tags';
-
-export const NAVIGATION_LINKS = [
-  { href: HOME_PATH, text: 'Home', Icon: HomeIcon },
-  { href: ABOUT_PATH, text: 'About', Icon: UserIcon },
-  { href: PROJECTS_PATH, text: 'Projects', Icon: ChecklistIcon },
-  { href: POSTS_PATH, text: 'Posts', Icon: ArticleIcon },
-  { href: CONTACT_PATH, text: 'Contact', Icon: MailIcon }
-];
