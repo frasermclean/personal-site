d1a4f1f3ff74896ad9246ba0d7925569d36ee5c4
Fraser McLean <dev@frasermclean.com>
2026-05-07 09:57:29 +0100

    Refactor getWebMentions action: filter out ignored authors and clean up unicode characters in names and content

--- src/actions/get-webmentions.ts
+++ src/actions/get-webmentions.ts
@@ -2,6 +2,8 @@ import type { WebMentionResponse } from '@/lib/webmention-types';
 import { z } from 'astro/zod';
 import { ActionError, defineAction } from 'astro:actions';
 
+const authorUrlsToIgnore = ['https://reddit.com/user/asimovs-auditor/'];
+
 export const getWebMentions = defineAction({
   input: z.object({
     slug: z.string()
@@ -20,7 +22,19 @@ export const getWebMentions = defineAction({
 
     try {
       const data = (await response.json()) as WebMentionResponse;
-      return data.children;
+      const entries = data.children
+        .filter((entry) => !authorUrlsToIgnore.includes(entry.author.url))
+        .map((entry) => ({
+          ...entry,
+          author: {
+            ...entry.author,
+            name: entry.author.name.replaceAll('????', '') // unicode / emoji characters seem to appear as ???? so just remove them
+          },
+          content: {
+            text: entry.content?.text ? entry.content.text.replaceAll('????', '') : undefined
+          }
+        }));
+      return entries;
     } catch (error) {
       console.error('Error parsing web mentions response:', error);
       throw new ActionError({
