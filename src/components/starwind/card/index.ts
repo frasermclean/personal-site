778a6174a2e50b1aaa100362a4064f58d19becb6
Fraser McLean <dev@frasermclean.com>
2026-05-11 08:19:11 +0100

    Update Starwind card components

--- src/components/starwind/card/index.ts
+++ src/components/starwind/card/index.ts
@@ -1,20 +1,22 @@
-import Card, { card } from "./Card.astro";
-import CardContent, { cardContent } from "./CardContent.astro";
-import CardDescription, { cardDescription } from "./CardDescription.astro";
-import CardFooter, { cardFooter } from "./CardFooter.astro";
-import CardHeader, { cardHeader } from "./CardHeader.astro";
-import CardTitle, { cardTitle } from "./CardTitle.astro";
+import Card, { card } from './Card.astro';
+import CardAction, { cardAction } from './CardAction.astro';
+import CardContent, { cardContent } from './CardContent.astro';
+import CardDescription, { cardDescription } from './CardDescription.astro';
+import CardFooter, { cardFooter } from './CardFooter.astro';
+import CardHeader, { cardHeader } from './CardHeader.astro';
+import CardTitle, { cardTitle } from './CardTitle.astro';
 
 const CardVariants = {
   card,
+  cardAction,
   cardContent,
   cardDescription,
   cardFooter,
   cardHeader,
-  cardTitle,
+  cardTitle
 };
 
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, CardVariants };
+export { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, CardVariants };
 
 export default {
   Root: Card,
@@ -23,4 +25,5 @@ export default {
   Title: CardTitle,
   Description: CardDescription,
   Content: CardContent,
+  Action: CardAction
 };
