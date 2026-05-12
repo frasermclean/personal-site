af5563d4cf3f64b2b4f1f78f305b17df81c763ba
Fraser McLean <dev@frasermclean.com>
2026-05-12 12:11:40 +0100

    Add CF worker types and generation script

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="astro/client" />
 /// <reference types="@types/umami" />
-
-declare module 'cloudflare:workers' {
-	export const env: {
-		SESSION?: KVNamespace;
-		[key: string]: unknown;
-	};
-}
