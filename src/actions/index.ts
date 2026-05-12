ef441d90b123c67d794bb3b4fba9e050a991d985
Fraser McLean <dev@frasermclean.com>
2026-05-12 16:50:26 +0100

    Remove GitHub login handlers from server action exports

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,7 @@
 import { processContactForm } from './contact-form';
 import { getWebMentions } from './get-webmentions';
-import { handleGithubCallback } from './handle-github-callback';
-import { initiateGithubLogin } from './initiate-github-login';
 
 export const server = {
   processContactForm,
-  getWebMentions,
-  initiateGithubLogin,
-  handleGithubCallback
+  getWebMentions
 };
