72ca07ab2399d36ffe7262b9e5ba08a1c2f8797f
Fraser McLean <dev@frasermclean.com>
2026-05-13 14:08:09 +0100

    Add OWNER_GITHUB_ID and POST_SYNDICATION bindings to configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -61,7 +61,8 @@ export default defineConfig({
       WORKERS_CI_COMMIT_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
       GITHUB_CLIENT_ID: envField.string({ context: 'server', access: 'public' }),
       GITHUB_CLIENT_SECRET: envField.string({ context: 'server', access: 'secret' }),
-      GITHUB_REDIRECT_URI: envField.string({ context: 'server', access: 'public' })
+      GITHUB_REDIRECT_URI: envField.string({ context: 'server', access: 'public' }),
+      OWNER_GITHUB_ID: envField.string({ context: 'server', access: 'secret', optional: true })
     }
   },
   integrations: [sitemap({ customSitemaps: [`${SITE_URL}/sitemap-posts.xml`] }), mdx()],
