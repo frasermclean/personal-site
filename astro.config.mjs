9f47e9025313b492282a1d327a2fcfd60b95c1e9
Fraser McLean <dev@frasermclean.com>
2026-05-13 09:27:33 +0100

    Update analytics environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,20 +31,16 @@ export default defineConfig({
   }),
   env: {
     schema: {
-      TRACKING_WEBSITE_ID: envField.string({ context: 'client', access: 'public' }),
-      TRACKING_SCRIPT_SRC: envField.string({
+      ANALYTICS_WEBSITE_ID: envField.string({ context: 'client', access: 'public' }),
+      ANALYTICS_SCRIPT_SRC: envField.string({
         context: 'client',
         access: 'public',
         default: 'http://localhost:8081/script.js'
       }),
       CONTACT_EMAIL: envField.string({ context: 'server', access: 'secret', default: '' }),
       RESEND_API_KEY: envField.string({ context: 'server', access: 'secret', default: '' }),
-      TURNSTILE_SITE_KEY: envField.string({ context: 'client', access: 'public', default: '1x00000000000000000000AA' }),
-      TURNSTILE_SECRET_KEY: envField.string({
-        context: 'server',
-        access: 'secret',
-        default: '1x0000000000000000000000000000000AA'
-      }),
+      TURNSTILE_SITE_KEY: envField.string({ context: 'client', access: 'public' }),
+      TURNSTILE_SECRET_KEY: envField.string({ context: 'server', access: 'secret' }),
       WORKERS_CI_COMMIT_SHA: envField.string({ context: 'client', access: 'public', default: '' }),
       GITHUB_CLIENT_ID: envField.string({ context: 'server', access: 'public', default: '' }),
       GITHUB_CLIENT_SECRET: envField.string({ context: 'server', access: 'secret', default: '' }),
