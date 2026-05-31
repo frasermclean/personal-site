4d7fc62644ec1fb858b7031de2e51009ca52e231
Fraser McLean <dev@frasermclean.com>
2026-05-31 09:20:37 +0100

    Remove no longer needed API endpoints

--- src/pages/api/auth/logout.ts
+++
@@ -1,7 +0,0 @@
-import { AuthMessage } from '@/lib/auth/auth-types';
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = async (context) => {
-  context.session?.destroy();
-  return context.redirect(`/?auth=${AuthMessage.LogoutSuccess}`);
-};
